58d7aaaa7d67fc3334b7cc0f793974e7ce73187a
Hector <hectordm65@gmail.com>
2019-06-24 18:46:58 -0400

    Started problem to add 2 numbers in linkedlist

---
+++ add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stddef.h>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// given 2 lists, add numbers from left to right
+// list is backwards
+// ex: 2 > 3 > 4, 5 > 3 > 7
+// 432 + 735 = 1167
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *my_answer = NULL;
+
+        return my_answer;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
