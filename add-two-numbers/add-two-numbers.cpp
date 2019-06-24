b130e3c52f4a3bd6bd0354a520be5a68c9d0b202
Hector <hectordm65@gmail.com>
2019-06-24 19:03:35 -0400

    Started the algorithm for adding the numbers

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -16,11 +16,37 @@ struct ListNode
 class Solution
 {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    ListNode *addTwoNumbers(ListNode *num_one, ListNode *num_two)
     {
         ListNode *my_answer = NULL;
+        int single_place = 0;
+        int next_place = 0;
 
-        return my_answer;
+        while (num_one->val != NULL || num_two->val != NULL)
+        {
+            single_place = num_one->val + num_one->val + next_place;
+            next_place = 0;
+            if (single_place > 9) // highest possible output is 18
+            {
+                my_answer->val = single_place - 10;
+                next_place = 1;
+            }
+            else // addition is 9 or less
+            {
+                my_answer->val = single_place;
+            }
+        }
+
+        if (num_one->val == NULL && num_two->val == NULL)
+        {
+            return my_answer;
+        }
+        else if (num_one->val == NULL)
+        {
+        }
+        else if (num_two->val == NULL) // not sure if I need this check, by deduction this is it
+        {
+        }
     }
 };
 
